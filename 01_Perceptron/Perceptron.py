8a456de81ee43eb3e6b887edf32c39f15c54dce6
Zygmut <deltas.game@gmail.com>
2023-09-21 19:09:57 +0200

    base perceptron implementation

--- 01_Perceptron/Perceptron.py
+++ 01_Perceptron/Perceptron.py
@@ -1,8 +1,9 @@
+
+from dataclasses import dataclass
 import numpy as np
 
-
+@dataclass
 class Perceptron:
-
     """Perceptron classifier.
 
     Parameters
@@ -21,40 +22,46 @@ class Perceptron:
 
     """
 
-    def __init__(self, eta=0.01, n_iter=10):
-        self.eta = eta
-        self.n_iter = n_iter
-        self.w_ = None  # defined in method fit
+    eta: float = 0.01
+    n_iter: int = 10
+    w: np.ndarray[np.float64] = None
 
-    def fit(self, X, y):
+
+    def fit(self, data, y):
 
         """Fit training data.
 
         Parameters
         ----------
-        X : {array-like}, shape = [n_samples, n_features]
-            Training vectors, where n_samples is the number of samples and
-            n_features is the number of features.
-        y : array-like, shape = [n_samples]
-            Target values.
+        Xdata : {array-like}, shape = [n_samples, n_features]
+                Training vectors, where n_samples is the number of samples and
+                n_features is the number of features.
+        y :     array-like, shape = [n_samples]
+                Target values.
 
         Returns
         -------
         self : object
 
         """
-        self.w_ = np.zeros(1 + X.shape[1])  # First position corresponds to threshold
 
-        # TODO: Put your code (fit algorithm)
+        self.w = np.zeros(1 + data.shape[1])  # First position corresponds to threshold
 
+        for _ in range(self.n_iter):
 
-    def predict(self, X):
+            for x_it, y_it in zip(data, y):
+                delta_w = self.eta * (y_it - self.predict(x_it))
+
+                self.w[0] += delta_w
+                self.w[1:] += delta_w * x_it
+
+    def predict(self, x):
         """Return class label.
             First calculate the output: (X * weights) + threshold
             Second apply the step function
             Return a list with classes
         """
 
-        # TODO: Put your code
+        res = np.dot(x, self.w[1:]) + self.w[0]
 
-        return np.random.randint(0, 2, size=X.shape[0])  # remove
+        return np.where(res >= 0, 1, -1)
--- 01_Perceptron/main.py
+++ 01_Perceptron/main.py
@@ -1,21 +1,36 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import make_classification
 from Perceptron import Perceptron
 
-# Generació del conjunt de mostres
-X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_repeated=0,
-                           n_classes=2, n_clusters_per_class=1, class_sep=1.25,
-                           random_state=0)
+def linear_fn(w):
+    slope = -(w[0] / w[2]) / (w[0] / w[1])
+    intercept = -w[0] / w[2]
 
-y[y == 0] = -1  # La nostra implementació esta pensada per tenir les classes 1 i -1.
+    return lambda x: slope * x + intercept
 
+def run():
+    # Generació del conjunt de mostres
+    x, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_repeated=0,
+                               n_classes=2, n_clusters_per_class=1, class_sep=1.25,
+                               random_state=0)
 
-perceptron = Perceptron()
-perceptron.fit(X, y)  # Ajusta els pesos
-y_prediction = perceptron.predict(X)  # Prediu
+    y[y == 0] = -1  # La nostra implementació esta pensada per tenir les classes 1 i -1.
 
-#  Resultats
-plt.figure(1)
-plt.scatter(X[:, 0], X[:, 1], c=y_prediction)  # Mostram el conjunt de mostres el color indica la classe
-plt.show()
+    perceptron = Perceptron()
+    perceptron.fit(x, y)
+    y_prediction = perceptron.predict(x)
+
+    weight_fn = linear_fn(perceptron.w)
+    x_plot = np.linspace(np.max(x) + 1, np.min(x) - 1)
+    y_plot = weight_fn(x_plot)
+
+    #  Resultats
+    plt.figure(1)
+    plt.title("Perceptron")
+    plt.plot(x_plot, y_plot, color="black")
+    plt.scatter(x[:, 0], x[:, 1], c=y_prediction)  # Mostram el conjunt de mostres el color indica la classe
+    plt.show()
+
+if __name__ == "__main__":
+    run()
